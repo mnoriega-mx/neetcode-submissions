818e53446ac8e3369d6d8af92f5d45b3be48efc9
Mauricio Noriega <44309779+mnoriega-mx@users.noreply.github.com>
2026-08-24 23:05:19 +0200

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = []
+        
+        for i in s:
+            if i.isalnum():
+                clean.append(i.lower())
+        clean = ''.join(clean)
+             
+        l, r = 0, len(clean)-1
+
+        while l < r:
+            if clean[l] != clean[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
